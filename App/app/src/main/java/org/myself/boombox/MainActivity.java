14734f39f7be94e59158442fdba8de59dde866a5
Ruiz <08009004@live.napier.ac.uk>
2017-02-03 15:14:02 +0000

    playing mp3 file on virtual machine successfully (With MediaPlayer object) TO DO: follow tutorial @ https://developer.android.com/guide/topics/media/mediaplayer.html

--- App/app/src/main/java/org/myself/boombox/MainActivity.java
+++ App/app/src/main/java/org/myself/boombox/MainActivity.java
@@ -1,9 +1,12 @@
 package org.myself.boombox;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -34,5 +37,11 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void handleClickPlay(View v) {
+        MediaPlayer mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.brass_gavel);
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mPlayer.start();
     }
 }
